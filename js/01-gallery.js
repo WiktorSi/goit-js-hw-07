67fb3284830e30104ba2738ced907aa915d39824
WiktorSi <vitek2@poczta.onet.pl>
2023-03-01 17:52:03 +0100

    HO-7-part III

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,6 @@ galleryItems.forEach((item) => {
     const galleryItem = `<div class = "gallery__item"><a class="gallery__link" href="${item.original}"><img class = "gallery__image" src="${item.preview}" data-source = "${item.original}" alt="${item.description}"</a></div>`
     gallery.insertAdjacentHTML('beforeend', galleryItem);
 })
-console.log(galleryItems);
 
 gallery.addEventListener('click', (event) => {
     event.preventDefault();
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,7 @@ galleryItems.forEach((item) => {
     const galleryItem = `<div><a class="gallery__item" href="${item.original}"><img class = "gallery__image" src="${item.preview}" alt="${item.description}"</a></div>`
     gallery.insertAdjacentHTML('beforeend', galleryItem);
 })
-console.log(galleryItems);
+
 
     const lightbox = new SimpleLightbox('.gallery a', { 
   caption: true,
